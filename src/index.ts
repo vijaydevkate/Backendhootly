569f1164d7a3982bdc65f576d10361425e6a34be
vijaydevkate <vijaydevkate98@gmail.com>
2026-01-06 12:19:55 -0500

    Signup end point created and is working

--- src/index.ts
+++ src/index.ts
@@ -6,26 +6,31 @@ import { userModel } from './db.js';
 const app = express();
 app.use(express.json());
 
-app.post('/signup', (req, res) => {
+app.post('/signup', async (req, res) => {
 // Todo: Zod validation, hash the password
 
 const username = req.body.username; //Getting Usernname
 const password = req.body.password; //Getting password
 
 //Create a username and password entry in DB
-  userModel.create({
+try{
+  await userModel.create({
     username: username,
     password: password
 })
 
-res.json({
+res.status(200).json({
     message: "User Signed up succesfully"
 })
-
-
+}catch(e){
+    res.status(411).json({
+        message:"user already exists"
+    })
+}
 })
 
 app.post('/signin', (req, res) => {
+ 
  
 })
 
