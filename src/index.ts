416d831585a08b1844be9e23a9753ba1000259ae
vijaydevkate <vijaydevkate98@gmail.com>
2026-01-06 11:14:46 -0500

    route created

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import jwt from 'jsonwebtoken'
 
 const app = express();
 
+app.post('/signup', (req, res) => {
+
+})
+
+app.post('/signin', (req, res) => {
+ 
+})
+
+
+app.listen(3000);
