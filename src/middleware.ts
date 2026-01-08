24f13777ae0a529b2e7cee8ac53b17f6e49dbaa0
vijaydevkate <vijaydevkate98@gmail.com>
2026-01-08 18:35:15 -0500

    Definging header in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,13 @@
 import type {Request, Response, NextFunction} from 'express';
+import jwt from 'jsonwebtoken'
+
+const JWT_PASSWORD = "!23123"
 
 export const userMiddleware = (req : Request, res: Response, next: NextFunction) => {
+    //get header
+    const header = req.header('Authorization')
+
+    //Decoding the header
+    const decode = jwt.verify(header as string, JWT_PASSWORD)
 
 }
